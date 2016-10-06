679094c3af060bb4b1bd6e774318021d80960a3a
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-06 22:08:46 +0200

    [ACC-485] add some sleep before requesting rbmq server

--- roles/rabbitmq/templates/add_guest_user.sh
+++ roles/rabbitmq/templates/add_guest_user.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sleep 5
+
 curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"password":"guest","tags":"None"}' http://{{ansible_eth0.ipv4.address}}:15672/api/users/guest
 
 curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"configure":".*","write":".*","read":".*"}' http://{{ansible_eth0.ipv4.address}}:15672/api/permissions/%2F/guest
