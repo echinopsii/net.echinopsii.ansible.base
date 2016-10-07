88b2c8c97e4cdba33c6fa746f7f3a44cf3be80ae
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-07 15:36:33 +0200

    [ACC-485] improve rbmq user script

--- roles/rabbitmq/templates/add_guest_user.sh
+++ roles/rabbitmq/templates/add_guest_user.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-sleep 5
+ret=-1
+i="0"
+while [ $ret -ne 0 ]
+do
+	curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"password":"guest","tags":"None"}' http://{{ansible_eth0.ipv4.address}}:15672/api/users/guest
+	ret=$?
+	if [[ $i -lt 4 ]] && [[ $ret -ne 0 ]]; then
+		sleep 2
+	else
+		break;
+	fi
+done
 
-curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"password":"guest","tags":"None"}' http://{{ansible_eth0.ipv4.address}}:15672/api/users/guest
-
-curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"configure":".*","write":".*","read":".*"}' http://{{ansible_eth0.ipv4.address}}:15672/api/permissions/%2F/guest
+if [[ $ret -eq 0 ]]; then
+	curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"configure":".*","write":".*","read":".*"}' http://{{ansible_eth0.ipv4.address}}:15672/api/permissions/%2F/guest
+fi
