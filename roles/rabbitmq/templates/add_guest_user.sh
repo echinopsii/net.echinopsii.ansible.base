b7bafa41779760c831271242a5e402e9aab1f6b7
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-15 20:04:18 +0200

    [ACC-485] update playbooks and roles for ansible v2

--- roles/rabbitmq/templates/add_guest_user.sh
+++ roles/rabbitmq/templates/add_guest_user.sh
@@ -4,7 +4,7 @@ ret=-1
 i=0
 while [ $ret -ne 0 ]
 do
-	curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"password":"guest","tags":"None"}' http://{{ansible_eth0.ipv4.address}}:15672/api/users/guest
+	curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"password":"guest","tags":"None"}' http://{{ansible_fqdn}}:15672/api/users/guest
 	ret=$?
 	if [[ $i -lt 15 ]] && [[ $ret -ne 0 ]]; then
 		sleep 2
@@ -15,5 +15,5 @@ do
 done
 
 if [[ $ret -eq 0 ]]; then
-	curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"configure":".*","write":".*","read":".*"}' http://{{ansible_eth0.ipv4.address}}:15672/api/permissions/%2F/guest
+	curl --user admin:admin -H "Content-Type: application/json" -X PUT -d '{"configure":".*","write":".*","read":".*"}' http://{{ansible_fqdn}}:15672/api/permissions/%2F/guest
 fi
