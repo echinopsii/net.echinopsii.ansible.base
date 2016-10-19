b44709644cb24e8f44e14ce8b946de8ff295ee46
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-19 10:47:38 +0200

    [ACC-486] remove -ti

--- roles/ariane-demo/files/ariane-db-init.sh
+++ roles/ariane-demo/files/ariane-db-init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -it --name mysqlcli --link ariane-dir:ariane-dir -v /tmp:/tmp mysql sh -c "exec mysql -h
+docker run --name mysqlcli --link ariane-dir:ariane-dir -v /tmp:/tmp mysql sh -c "exec mysql -h
  ariane-dir -P 3306 -uariane -ppassword < /tmp/ariane-dir.sql"
