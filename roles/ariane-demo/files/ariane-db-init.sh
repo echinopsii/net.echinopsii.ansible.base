b20e151b8890ee8bf8b2bdadb93d88c286e4affa
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-19 12:15:39 +0200

    [ACC-485] add idm sql script

--- roles/ariane-demo/files/ariane-db-init.sh
+++ roles/ariane-demo/files/ariane-db-init.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker run --name mysqlcli --link ariane-dir:ariane-dir -v /tmp:/tmp mysql sh -c "exec mysql -h ariane-dir -P 3306 -uariane -ppassword < /tmp/ariane-dir.sql"
+docker run --name mysqlcli-idm --link ariane-idm:ariane-idm -v /tmp:/tmp mysql sh -c "exec mysql -h ariane-idm -P 3306 -uariane -ppassword < /tmp/ariane-idm.sql"
+
+docker run --name mysqlcli-dir --link ariane-dir:ariane-dir -v /tmp:/tmp mysql sh -c "exec mysql -h ariane-dir -P 3306 -uariane -ppassword < /tmp/ariane-dir.sql"
+
